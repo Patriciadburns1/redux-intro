ce672f9b1b2a9378c2ef2d6c74cd5d9940da7f67
dburns1 <patriciadburns@gmail.com>
2018-07-02 17:27:03 -0700

    redux intro adding the clock and making it tick

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,11 @@
 //take all reducers make single reducer 
-
 import {combineReducers} from 'redux'; 
 import clockReducer from './clock_reducer'; 
+import userReducer from './user_reducer'; 
 
 const rootReducer = combineReducers({
-    clock: clockReducer
+    clock: clockReducer, 
+    user: userReducer
 }); 
 
 export default rootReducer; 
